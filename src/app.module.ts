eb2163bb5ff684f73a3c36758cfb010212ea2dca
bibby <me@bibbythe.dev>
2026-09-16 02:18:52 +0100

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SentryModule } from '@sentry/nestjs/setup';
+
+@Module({
+  imports: [SentryModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/instrument.ts
@@ -0,0 +1,17 @@
+// Import with `const Sentry = require("@sentry/nestjs");` if you are using CJS
+import * as Sentry from '@sentry/nestjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: 'https://82374e23455e1c686ea346a7e06201c3@sentry.bibbythe.dev/6',
+  integrations: [nodeProfilingIntegration()],
+
+  // Send structured logs to Sentry
+  enableLogs: true,
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
+});
